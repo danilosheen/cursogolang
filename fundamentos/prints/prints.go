8cf153620e2af31609af7e922627fb08859f1e9b
danilosheen <c.danilo.f.silva@gmail.com>
2023-02-18 18:35:01 -0300

    prints e tipos de dados

---
+++ fundamentos/prints/prints.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Print("Mesma")
+	fmt.Print(" linha.")
+
+	fmt.Println(" Nova")
+	fmt.Println("linha.")
+
+	x := 3.141516
+
+	//como o valor de x é um float, o go não faz concatenação com string
+	// fmt.Println("O valor de x é " + x)
+	//uma alternativa é usar uma vírgula para exibir o valor de x como floar após
+	fmt.Println("O valor de x é", x)
+
+	//outra alternativa é criar uma nova variável e transformar o x em uma string
+	xs := fmt.Sprint(x)
+	fmt.Println("O valor de x é " + xs)
+
+	/* 
+	o printf é a maneira de imprimir com formatações, um exemplo é definir casas
+	decimais
+	*/
+
+	fmt.Printf("O valor de x é %.2f", x)
+
+	/*
+	%f - tipo float
+	%s - tipo string
+	%d - tipo inteiro
+	%t - tipo boolean
+	%v - é um wildcard e imprime corretamente diversos formatos
+	*/
+
+	a := 1
+	b := 1.9999
+	c := false
+	d := "string"
+
+	fmt.Printf("\n%d %f %.1f %t %s", a, b, b, c, d)
+	fmt.Printf("\n%v %v %v %v %v", a, b, b, c, d)
+
+}
