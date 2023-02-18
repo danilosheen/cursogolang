b0bc83e2c132d246d5f82676aedbeec9b922aac0
danilosheen <c.danilo.f.silva@gmail.com>
2023-02-18 20:00:51 -0300

    add math

--- fundamentos/prints/prints.go
+++ fundamentos/prints/prints.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main(){
+func main() {
 
 	fmt.Print("Mesma")
 	fmt.Print(" linha.")
@@ -10,7 +13,7 @@ func main(){
 	fmt.Println(" Nova")
 	fmt.Println("linha.")
 
-	x := 3.141516
+	x := math.Pi
 
 	//como o valor de x é um float, o go não faz concatenação com string
 	// fmt.Println("O valor de x é " + x)
@@ -21,24 +24,24 @@ func main(){
 	xs := fmt.Sprint(x)
 	fmt.Println("O valor de x é " + xs)
 
-	/* 
-	o printf é a maneira de imprimir com formatações, um exemplo é definir casas
-	decimais
+	/*
+		o printf é a maneira de imprimir com formatações, um exemplo é definir casas
+		decimais
 	*/
 
 	fmt.Printf("O valor de x é %.2f", x)
 
 	/*
-	%f - tipo float
-	%s - tipo string
-	%d - tipo inteiro
-	%t - tipo boolean
-	%v - é um wildcard e imprime corretamente diversos formatos
+		%f - tipo float
+		%s - tipo string
+		%d - tipo inteiro
+		%t - tipo boolean
+		%v - é um wildcard e imprime corretamente diversos formatos
 	*/
 
 	a := 1
-	b := 1.9999
-	c := false
+	b := 1.95
+	c := true
 	d := "string"
 
 	fmt.Printf("\n%d %f %.1f %t %s", a, b, b, c, d)
