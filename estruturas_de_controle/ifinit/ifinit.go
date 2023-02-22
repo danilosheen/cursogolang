c9219bf81c21b3dc13cfcc9722901b9ba0f01296
danilosheen <c.danilo.f.silva@gmail.com>
2023-02-22 12:53:24 -0300

    usando variável no init da função

---
+++ estruturas_de_controle/ifinit/ifinit.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func numeroAleatorio() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	return r.Intn(10)
+}
+
+
+func main()  {
+	if i := numeroAleatorio(); i > 5{ //também suportado no switch
+		fmt.Println("Ganhou!!!")
+		fmt.Println(i)
+	
+	} else {
+		fmt.Println("Você perdeu!")
+		fmt.Println(i)
+	}
+}
